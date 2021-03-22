d8c67f10e380e4611f5c078553bc3520d5c2a0c4
Michael X. Grey <grey@openrobotics.org>
2021-03-22 23:42:55 +0800

    Finish LaneClosure class and write unit test
    Signed-off-by: Michael X. Grey <grey@openrobotics.org>

--- rmf_traffic/src/rmf_traffic/agv/LaneClosure.cpp
+++ rmf_traffic/src/rmf_traffic/agv/LaneClosure.cpp
@@ -16,7 +16,7 @@
 */
 
 #include <rmf_traffic/agv/LaneClosure.hpp>
-#include <vector>
+#include <unordered_map>
 
 namespace rmf_traffic {
 namespace agv {
@@ -26,25 +26,117 @@ class LaneClosure::Implementation
 {
 public:
 
-  std::vector<std::size_t> bitfield;
-
   bool contains(std::size_t value) const
   {
-    const std::size_t bucket = value/sizeof(std::size_t);
-    if (bitfield.size() <= bucket)
+    assert(field_size == 64);
+    const std::size_t key = _get_key(value);
+    const auto bucket_it = _bitfields.find(key);
+    if (bucket_it == _bitfields.end())
       return false;
 
-
-    const std::size_t compare = 1 << lane;
-    return static_cast<bool>(_pimpl->hash & compare);
+    const std::size_t bit = _get_bit(value);
+    const std::size_t bitfield = bucket_it->second;
+    return static_cast<bool>(bitfield & bit);
   }
 
+  void insert(std::size_t value)
+  {
+    const std::size_t key = _get_key(value);
+    const std::size_t bit = _get_bit(value);
+
+    const auto insertion = _bitfields.insert({key, bit});
+    if (insertion.second)
+    {
+      // The bitfield did not exist before, so now it has been inserted with
+      // the desired bit. We will need to recalculate the hash. After that, we
+      // can return.
+      _recalculate_hash();
+      return;
+    }
+
+    std::size_t& bitfield = insertion.first->second;
+    if (static_cast<bool>(bitfield & bit))
+    {
+      // The map already contained this value, so we do not need to insert it
+      // or recalculate the hash.
+      return;
+    }
+
+    bitfield |= bit;
+    _recalculate_hash();
+  }
+
+  void erase(std::size_t value)
+  {
+    const std::size_t key = _get_key(value);
+    const auto bucket_it = _bitfields.find(key);
+    if (bucket_it == _bitfields.end())
+    {
+      // There was no bitfield for this value, so it is not in the map anyway.
+      // We can simply return here.
+      return;
+    }
+
+    const std::size_t bit = _get_bit(value);
+    std::size_t& bitfield = bucket_it->second;
+    if (!static_cast<bool>(bitfield & bit))
+    {
+      // This bit was not active in the bitfield, so we do not need to do
+      // anything.
+      return;
+    }
+
+    bitfield &= ~bit;
+    _recalculate_hash();
+  }
+
+  std::size_t hash() const
+  {
+    return _hash;
+  }
+
+  bool operator==(const Implementation& other) const
+  {
+    return _bitfields == other._bitfields;
+  }
+
+private:
+
+  std::unordered_map<std::size_t, std::size_t> _bitfields;
+  std::size_t _hash = 0;
+
+  static constexpr std::size_t field_size = sizeof(std::size_t)*8;
+
+  void _recalculate_hash()
+  {
+    _hash = 0;
+    for (const auto& [_, bitfield] : _bitfields)
+      _hash |= bitfield;
+  }
+
+  std::size_t _get_key(const std::size_t value) const
+  {
+    return value/field_size;
+  }
+
+  std::size_t _get_bit(const std::size_t value) const
+  {
+    const std::size_t shift = value % field_size;
+    return std::size_t(1) << shift;
+  }
 };
+
+//==============================================================================
+LaneClosure::LaneClosure()
+: _pimpl(rmf_utils::make_impl<Implementation>())
+{
+  // Do nothing
+}
 
 //==============================================================================
 bool LaneClosure::is_open(const std::size_t lane) const
 {
-  return !_pimpl->contains(lane);
+  return !is_closed(lane);
 }
 
 //==============================================================================
@@ -56,29 +148,27 @@ bool LaneClosure::is_closed(const std::size_t lane) const
 //==============================================================================
 LaneClosure& LaneClosure::open(const std::size_t lane)
 {
-  const std::size_t bit = 1 << lane;
-  _pimpl->hash = (_pimpl->hash & ~bit);
+  _pimpl->erase(lane);
   return *this;
 }
 
 //==============================================================================
 LaneClosure& LaneClosure::close(std::size_t lane)
 {
-  const std::size_t bit = 1 << lane;
-  _pimpl->hash = (_pimpl->hash | bit);
+  _pimpl->insert(lane);
   return *this;
 }
 
 //==============================================================================
 std::size_t LaneClosure::hash() const
 {
-  return _pimpl->hash;
+  return _pimpl->hash();
 }
 
 //==============================================================================
 bool LaneClosure::operator==(const LaneClosure& other) const
 {
-
+  return *_pimpl == *other._pimpl;
 }
 
 } // namespace agv
--- rmf_traffic/test/unit/agv/test_LaneClosure.cpp
+++ rmf_traffic/test/unit/agv/test_LaneClosure.cpp
@@ -20,8 +20,9 @@
 #include <rmf_utils/catch.hpp>
 
 #include <unordered_set>
+#include <random>
 
-SCENARIO("Test LaneClosure class")
+SCENARIO("Test LaneClosure class functions")
 {
   rmf_traffic::agv::LaneClosure closure;
   const std::size_t lane_num = 100;
@@ -41,7 +42,7 @@ SCENARIO("Test LaneClosure class")
 //  WHEN("Lane 73 is closed")
   {
     closure.close(73);
-    CHECK(closure.hash() == (1 << 73));
+    CHECK(closure.hash() != 0);
 
 //    THEN("All lanes but 73 are open")
     {
@@ -59,5 +60,106 @@ SCENARIO("Test LaneClosure class")
         }
       }
     }
+  }
+
+//  WHEN("Lane 73 is reopened")
+  {
+    closure.open(73);
+    CHECK(closure.hash() == 0);
+
+//    THEN("All lanes are open")
+    {
+      for (std::size_t i=0; i < lane_num; ++i)
+      {
+        CHECK(closure.is_open(i));
+        CHECK_FALSE(closure.is_closed(i));
+      }
+    }
+  }
+
+  std::unordered_set<rmf_traffic::agv::LaneClosure> unique_closures;
+
+  closure.close(0);
+  CHECK(closure.hash() == 1);
+  CHECK_FALSE(closure.is_open(0));
+  CHECK(closure.is_closed(0));
+
+  unique_closures.insert(closure);
+
+  closure.close(64);
+  {
+    // NOTE(MXG): This is an implementation-specific expectation which can be
+    // relaxed if the implementation of the hash function is ever changed in the
+    // future.
+    CHECK(closure.hash() == 1);
+  }
+  CHECK_FALSE(closure.is_open(0));
+  CHECK(closure.is_closed(0));
+  CHECK_FALSE(closure.is_open(64));
+  CHECK(closure.is_closed(64));
+
+  // Since the value of the closure has changed, it should occupy a new spot in
+  // the set of unique closures, even though its hash has not changed.
+  unique_closures.insert(closure);
+  CHECK(unique_closures.size() == 2);
+
+  closure.open(64);
+  unique_closures.insert(closure);
+  // We should just be inserting the same closure value that we did originally,
+  // so there should still only be 2 closures in the unique set.
+  CHECK(unique_closures.size() == 2);
+}
+
+SCENARIO("Fuzz test of LaneClosure", "[debug]")
+{
+  const std::size_t num_lanes = 150;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<std::size_t> dist(0, num_lanes);
+
+  std::unordered_set<std::size_t> closed_lanes;
+  const std::size_t num_closed_lanes = 10;
+  while (closed_lanes.size() < num_closed_lanes)
+  {
+    closed_lanes.insert(dist(gen));
+  }
+
+  rmf_traffic::agv::LaneClosure closure;
+  std::unordered_set<rmf_traffic::agv::LaneClosure> unique_closures;
+  for (const auto& v : closed_lanes)
+  {
+    unique_closures.insert(closure);
+    closure.close(v);
+  }
+
+  // There should be one unique closure in this set for each lane that we have
+  // closed.
+  CHECK(unique_closures.size() == num_closed_lanes);
+
+  while (closed_lanes.size() > 0)
+  {
+    for (std::size_t i=0; i < num_lanes; ++i)
+    {
+      if (closed_lanes.count(i))
+      {
+        CHECK_FALSE(closure.is_open(i));
+        CHECK(closure.is_closed(i));
+      }
+      else
+      {
+        CHECK(closure.is_open(i));
+        CHECK_FALSE(closure.is_closed(i));
+      }
+    }
+
+    const auto pop = closed_lanes.begin();
+    closure.open(*pop);
+    closed_lanes.erase(pop);
+  }
+
+  for (std::size_t i=0; i < num_lanes; ++i)
+  {
+    CHECK(closure.is_open(i));
+    CHECK_FALSE(closure.is_closed(i));
   }
 }
