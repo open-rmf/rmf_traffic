af71eda7adaa5705f0af0179155d529093491a89
Arjo Chakravarty <arjo@openrobotics.org>
2021-06-07 11:46:42 +0800

    Add tests
    Signed-off-by: Arjo Chakravarty <arjo@openrobotics.org>

--- rmf_traffic/test/unit/reservations/internal/test_State.cpp
+++ rmf_traffic/test/unit/reservations/internal/test_State.cpp
@@ -19,3 +19,37 @@
 
 #include <rmf_utils/catch.hpp>
 
+using namespace rmf_traffic;
+using namespace rmf_traffic::reservations;
+using namespace std::chrono_literals;
+
+SCENARIO("A few reservations in a state")
+{
+    auto queue = std::make_shared<RequestQueue>();
+    State start_state(queue);
+
+    auto now = std::chrono::steady_clock::now();
+    now -= now.time_since_epoch();
+
+    auto request1_alt1 = ReservationRequest::make_request(
+        "table_at_timbre",
+        ReservationRequest::TimeRange::make_time_range(
+            now,
+            now+10s
+        ),
+        {10s}
+    );
+
+    auto request1 = std::vector{request1_alt1};
+    queue->enqueue_reservation(0, 0, 1, request1);
+    start_state = start_state.add_request(0, 0);
+
+    std::cout << "Start State:" <<std::endl;
+    start_state.debug_state();
+
+    for(auto next_state: start_state)
+    {
+        std::cout << "Next State:" <<std::endl;
+        next_state.debug_state();
+    }
+}
